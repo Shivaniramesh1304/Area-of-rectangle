6ddc1956d59dbbc22d258bde69e7e83646052b82
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:23:55 +0530

    Add files via upload

---
+++ Areaofrectangle.java
@@ -0,0 +1,10 @@
+public class Areaofrectangle {
+    public static void main(String[ ]args){
+        int length=12;
+        int breadth=2;
+        int area=length*breadth;
+        System.out.println("the area of rectangleis:"+area);
+    }
+}    
+    
+
